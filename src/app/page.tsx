ac1544184ae99dc7410894330ca195f4bb05bd88
jhonatanCastor <jhonatanjhonatan907@gmail.com>
2024-01-20 21:38:51 -0300

    feature: create front with data the user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,31 @@
 
-export default function Home() {
+import { prisma } from "@/lib/prisma";
+
+export default async function Home() {
+  let users = await prisma.user.findMany();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
+    <main style={{ maxWidth: 1200, marginInline: "auto", padding: 20 }}>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr 1fr",
+          gap: 20,
+        }}
+      >
+        {users.map((user) => (
+          <div
+            key={user.id}
+            style={{ border: "1px solid #ccc", textAlign: "center" }}
+          >
+            <img
+              src={`https://robohash.org/${user.id}?set=set2&size=180x180`}
+              alt={user.name}
+              style={{ height: 180, width: 180 }}
+            />
+            <h3>{user.name}</h3>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
