07afa20db35db7d297299d4d0c0b1a7f6095a922
jhonatanCastor <jhonatanjhonatan907@gmail.com>
2024-01-27 19:22:41 -0300

    feature: change seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,13 +3,14 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.upsert({
+  const user = await prisma.users.upsert({
     where: { email: "admin@admin.com" },
     update: {},
     create: {
       name: "Admin",
       email: "admin@admin.com",
       role: "admin",
+      password: 'Jhon999',
       description: 'test',
     },
   });
